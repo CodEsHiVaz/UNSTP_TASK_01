768040f045841106a0f68bad3f3083ffb3bf7f43
Shivaji z <99667776+CodEsHiVaz@users.noreply.github.com>
2023-07-13 12:11:27 +0530

    loader added

--- app/page.js
+++ app/page.js
@@ -10,6 +10,7 @@ export default function Home() {
   const [showToast, setShowToast] = useState(false);
   const [toastMessage, setToastMessage] = useState("");
   const [toastType, setToastType] = useState("");
+  const [isloading, setIsLoading] = useState(false);
 
   const showToastMessage = (message, type) => {
     setToastMessage(message);
@@ -20,23 +21,27 @@ export default function Home() {
     }, 3000);
   };
   const getdata = async () => {
+    setIsLoading(true);
     const res = await axios.get("api/seats");
     const data = await res.data.seatstatus;
     setseats(data);
+    setIsLoading(false);
   };
   const reset = async () => {
+    setIsLoading(true);
     try {
       const res = await axios.patch("api/seats/reset");
-      const data = await res.data.data;
+
       getdata();
       showToastMessage(`Booking reset successfully!`, "success");
     } catch (error) {
-
       console.log("reset  error:", error);
       showToastMessage("Failed while reset booking", "error");
     }
+    setIsLoading(false);
   };
   const handelSubmit = async (e) => {
+    setIsLoading(true);
     e.preventDefault();
     try {
       const res = await axios.post("api/seats/book", {
@@ -46,75 +51,93 @@ export default function Home() {
       getdata();
       showToastMessage(`${numOfSeats} Seat booked successfully!`, "success");
     } catch (error) {
-      console.log("handelSubmit  error:", error);
-      showToastMessage("Failed to book seat. Please try again.", "error");
+      console.log("handelSubmit  error:", error.response.data.message);
+      showToastMessage(
+        error.response.data.message ==
+          "You cannot book more than 7 seats at time"
+          ? error.response.data.message
+          : "Failed to book seat. Please try again.",
+        "error"
+      );
     }
+    setIsLoading(false);
   };
   useEffect(() => {
     getdata();
   }, []);
   return (
-    <main className={styles.main}>
-      <div className={styles.parrent}>
-        <div className={styles.seatsSection}>
-          <div className={styles.seatContainer}>
-            {seats &&
-              seats.map((elem) => {
-                return (
-                  <div
-                    key={elem._id}
-                    id={elem.status ? styles.red : styles.green}
-                    className={styles.box}
+    <>
+      {isloading && <div class="circle-spin-2"></div>}
+      <main className={styles.main}>
+        <div className={styles.parrent}>
+          <div className={styles.seatsSection}>
+            <div className={styles.seatContainer}>
+              {seats &&
+                seats.map((elem, ind) => {
+                  return (
+                    <div
+                      key={elem._id}
+                      id={elem.status ? styles.red : styles.green}
+                      className={styles.box}
+                    >
+                      <p>{elem.seatNumber}</p>
+                    </div>
+                  );
+                })}
+            </div>
+            <div className={styles.index}>
+              <div>
+                {" "}
+                <div id={styles.red}></div>
+                <p> = Booked Seats</p>
+              </div>
+              <div>
+                {" "}
+                <div id={styles.green}></div>
+                <p> = Available Seats</p>
+              </div>
+            </div>{" "}
+          </div>
+          <div className={styles.bookingSection}>
+            <h2 className={styles.head}>Bus Ticket Booking System</h2>
+
+            <div className={styles.bookingForm}>
+              {" "}
+              <form onSubmit={handelSubmit}>
+                <div className={styles.inputSection}>
+                  <label htmlFor="seatnumbr">
+                    Enter Number of Seats to be book
+                  </label>
+                  <input
+                    type="number"
+                    required
+                    min={1}
+                    max={7}
+                    onChange={(e) => setnumOfSeats(e.target.value)}
+                    name="seatnumbr"
+                    id="seatnumbr"
+                  />
+                </div>
+
+                <div className={styles.butnDiv}>
+                  <button
+                    onClick={reset}
+                    className={styles.reset}
+                    type="button"
                   >
-                    <p>{elem.seatNumber}</p>
-                  </div>
-                );
-              })}
-          </div>
-          <div className={styles.index}>
-            <div>
-              {" "}
-              <div id={styles.red}>0</div>
-              <p>Booked Seats</p>
+                    Resete Booking
+                  </button>
+                  <button className={styles.submit} type="submit">
+                    Submit
+                  </button>
+                </div>
+              </form>
             </div>
-            <div>
-              {" "}
-              <div id={styles.green}>0</div>
-              <p>Available Seats</p>
-            </div>
-          </div>{" "}
-        </div>
-        <div className={styles.bookingSection}>
-          <h2 className={styles.head}>Bus Ticket Booking System</h2>
 
-          <div className={styles.bookingForm}>
-            {" "}
-            <form onSubmit={handelSubmit}>
-              <div className={styles.inputSection}>
-                <label htmlFor="seatnumbr">
-                  Enter Number of Seats to be book
-                </label>
-                <input
-                  type="number"
-                  onChange={(e) => setnumOfSeats(e.target.value)}
-                  name="seatnumbr"
-                  id="seatnumbr"
-                />
-              </div>
-
-              <div className={styles.butnDiv}>
-                <button onClick={reset} className={styles.reset} type="button">
-                  Resete Booking
-                </button>
-                <button className={styles.submit} type="submit">
-                  Submit
-                </button>
-              </div>
-            </form>
+            {showToast && <Toast message={toastMessage} type={toastType} />}
           </div>
-      {showToast && <Toast message={toastMessage} type={toastType} />}
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
